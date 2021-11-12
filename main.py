821d4d6e51757512a0008daf87d20cdae279a0ac
Rovastus <44978829+Rovastus@users.noreply.github.com>
2021-11-12 17:32:19 +0100

    Update main.py - nargs change to +

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import api_util as api
 BASE_URL = "http://169.51.194.78:31798/"
 
 cli = argparse.ArgumentParser()
-cli.add_argument("--maps", nargs="*", default=[],)
+cli.add_argument("--maps", nargs="+", default=[],)
 args = cli.parse_args()
 
 print("maps: %r" % args.maps)
