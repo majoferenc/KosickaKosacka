06fca9fa238bd762e22bdd787466069d86363792
Rovastus <44978829+Rovastus@users.noreply.github.com>
2021-11-12 22:18:21 +0100

    step method now use SupportedMove enum

--- api_util.py
+++ api_util.py
@@ -4,7 +4,7 @@ BASE_URL = "http://169.51.194.78:31798/"
 TEAM_NAME = "KošickeKosačky@IBM"
 
 def step(session_id, move):
-    reponse = requests.get(BASE_URL+"step/", params={"id": session_id, "move": move})
+    reponse = requests.get(BASE_URL+"step/", params={"id": session_id, "move": move.value})
     print(response.json())
     return reponse.json()
 
