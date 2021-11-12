22939084b2642c3fbc8c87c70bb51b5ce2f7c434
Rovastus <44978829+Rovastus@users.noreply.github.com>
2021-11-12 17:44:33 +0100

    fix def parameters name format to underscore

--- api_util.py
+++ api_util.py
@@ -3,13 +3,13 @@ import requests
 BASE_URL = "http://169.51.194.78:31798/"
 TEAM_NAME = "KošickeKosačky@IBM"
 
-def step(sessionid, move):
-    reponse = requests.get(BASE_URL+"step/", params={"id": sessionid, "move": move})
+def step(session_id, move):
+    reponse = requests.get(BASE_URL+"step/", params={"id": session_id, "move": move})
     print(response.json())
     return reponse.json()
 
 
-def init_session(mapName):
-    response = requests.get(BASE_URL+"init/", params={"mapName": mapName, "teamName": TEAM_NAME})
+def init_session(map_name):
+    response = requests.get(BASE_URL+"init/", params={"mapName": map_name, "teamName": TEAM_NAME})
     print(response.json())
     return response.json()
