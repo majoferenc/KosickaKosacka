7bc960f6f74234395de13c9e98336b76528fd4be
Marian-Ferenc2 <Marian.Ferenc2@ibm.com>
2021-11-13 13:37:44 +0100

    fix: bfs python errors

--- bfs.py
+++ bfs.py
@@ -1,28 +1,30 @@
 import queue
+import numpy as np
 from point import Point
 from sensor_response import SensorResponse
 from map import PositionState
 
-def bfs(start_point: Point, map: [Point, SensorResponse]):
-  queue = queue.Queue()
-  # v mape "predchodca" sa uklada nasledujuci bod v najkratsej ceste ku stanici
-  predchodca = []
-  directions = {}
-  passed = {}
-  queue.put(startPoint)
-  passed[0] = startPoint
 
-  arround = np.array([[1,1], [1,0], [1,-1], [0,-1], [-1,-1], [-1,0], [-1,1], [0,1]])
-  while True:
-    point = queue.remove()
-    if map[point] == SensorResponse.CUT or map[point] == SensorResponse.BORDER:
-      continue
-    for i in range(8):
-      neighbour = Point(start_point[x] + arround[i][0], start_point[y] + arround[i][1])
-      if map[neighbour] == "lawn_mower":
-        predchodca.add(neighbour, point)
-        return
-      if !passed.contais(neighbour):
-        queue.put(neighbour)
-        passed.add(neighbour)
-        predchodca.add(neighbour, point)
+def bfs(start_point: Point, map: [Point, SensorResponse]):
+    bfs_queue = queue.Queue()
+    # v mape "predchodca" sa uklada nasledujuci bod v najkratsej ceste ku stanici
+    predchodca = []
+    directions = {}
+    passed = {}
+    bfs_queue.put(start_point)
+    passed[0] = start_point
+
+    arround = np.array([[1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1]])
+    while True:
+        point = bfs_queue.empty()
+        if map[point] == SensorResponse.CUT or map[point] == SensorResponse.BORDER:
+            continue
+        for i in range(8):
+            neighbour = Point(start_point.X + arround[i][0], start_point.Y + arround[i][1])
+            if map[neighbour] == "lawn_mower":
+                directions[i] = (neighbour, point)
+                return
+            if not passed.get(neighbour):
+                bfs_queue.put(neighbour)
+                passed[i] = neighbour
+                predchodca[i] = neighbour, point
