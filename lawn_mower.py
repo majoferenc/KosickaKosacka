ca3912a063fb7a0e7cdcaa21f607d8c34c112785
Rovastus <44978829+Rovastus@users.noreply.github.com>
2021-11-14 00:01:19 +0100

    Lawn mower - get_supported_moves added

--- lawn_mower.py
+++ lawn_mower.py
@@ -1,5 +1,5 @@
 import dijkstra as dijkstra
-from supported_move import SupportedMove
+from supported_move import SupportedMove,  get_turns
 from point import Point
 from sensor_response import SensorResponse
 import dfs as dfs
@@ -8,7 +8,19 @@ import bfs as bfs
 def moves_to_exectute(map):
     if map.get_charger_position() is None:
         #use bfs
-        bfs.bfs(map.get_current_position(), map)
+        ponts, directions = bfs.bfs(map.get_current_position(), map)
+        return get_supported_moves(map.get_current_direction(), directions)
     else:
         #use dfs
-        dfs.dfs(map)
+        ponts, directions = dfs.dfs(map)
+        return get_supported_moves(map.get_current_direction(), directions)
+
+def get_supported_moves(current_direction, directions):
+    supported_moves =[]
+    current_direction_temp = current_direction
+    
+    for direction in directions:
+        supported_moves.extend(get_turns(current_direction_temp, direction))
+        supported_moves.append(SupportedMove.FORWARD)
+        current_direction_temp = direction
+    
