172a402e046c9a8f68799a6557a150a9cdeb89e4
Rovastus <44978829+Rovastus@users.noreply.github.com>
2021-11-13 10:34:40 +0100

    Argparse arguments updated

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from model import Model
 
 cli = argparse.ArgumentParser()
 cli.add_argument("--maps", nargs="+", default=[],)
-cli.add_argument("--base_url", nargs="+", default=["http://localhost/"])
-cli.add_argument("--render_mode", nargs="+", default=["False"])
+cli.add_argument("--base_url", nargs="?", type=str, default="http://localhost/")
+cli.add_argument("--render_mode", nargs="?", type=bool, default=False)
 args = cli.parse_args()
 
 print("maps: %r" % args.maps)
-print("base_url: %s" % str(args.base_url))
+print("base_url: %s" % args.base_url)
 print("render_mode: %s" % str(args.render_mode))
 
 for map in args.maps:
     print("map: %s" % map)
-    init_response, response_code = api.init_session(map, args.base_url[0])
-    model = Model(init_response, args.base_url[0], args.render_mode[0])
+    init_response, response_code = api.init_session(map, args.base_url)
+    model = Model(init_response, args.base_url, args.render_mode)
     model.execute()
