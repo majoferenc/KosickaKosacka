5d720acb8a530cd175481b47bf6e5c54a53689f1
Rovastus <44978829+Rovastus@users.noreply.github.com>
2021-11-13 16:54:47 +0100

    Supported move format

--- supported_move.py
+++ supported_move.py
@@ -10,20 +10,21 @@ class SupportedMove(str, Enum):
 def get_turns(start, end):
     supported_moves = []
     
-    right_clockwise = cycle([1,2,3,4,5,6,7,8])
-    left_clockwise = cycle([8,7,6,5,4,3,2,1])
-    right = turn_count(right_clockwise, start, end)
-    left = turn_count(left_clockwise, start, end)
+    right = turn_count(cycle([1,2,3,4,5,6,7,8]), start, end)
+    left = turn_count(cycle([8,7,6,5,4,3,2,1]), start, end)
+    
     if left < right:
         supported_moves.extend([SupportedMove.TURN_LEFT] * left)
     elif right < left:
         supported_moves.extend([SupportedMove.TURN_RIGHT] * right)
     else:
         supported_moves.extend([SupportedMove.TURN_LEFT] * left)
+    
     return supported_moves
         
 def turn_count(cycle_data, start, end):
     i = 0
+    
     for data in cycle_data:
         if i == 0 and data == start:
             i += 1
