889e15df02acad0c11e7505e384eb09868611e12
Ivan Rastislav Gavlak <igavlak@ngncc.de>
2021-11-13 23:07:18 +0100

    Return move in dfs.

--- dfs.py
+++ dfs.py
@@ -5,14 +5,13 @@ from map import Map
 
 
 
-def dfs():
-    map = Map.get_map()
+def dfs(map):
     pos = map.get_current_position()
     for i in [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]:
         step = Point(pos.X + i[0], pos.Y + i[1])
         # Pozicia este nebola preskumana
         if map.get_position_state(step) is None:
-            return step
+            return step, i
 
     return None
 
