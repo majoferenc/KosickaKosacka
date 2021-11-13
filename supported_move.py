f2bc6796b93c15023048a0f36403539c03bcbe96
Rovastus <44978829+Rovastus@users.noreply.github.com>
2021-11-13 23:45:05 +0100

    get_turns method now works with direction array

--- supported_move.py
+++ supported_move.py
@@ -9,9 +9,10 @@ class SupportedMove(str, Enum):
     
 def get_turns(start, end):
     supported_moves = []
-    
-    right = turn_count(cycle([1,2,3,4,5,6,7,8]), start, end)
-    left = turn_count(cycle([8,7,6,5,4,3,2,1]), start, end)
+    start_number = get_direction_number(start)
+    end_number = get_direction_number(end)
+    right = turn_count(cycle([1,2,3,4,5,6,7,8]), start_number, end_number)
+    left = turn_count(cycle([8,7,6,5,4,3,2,1]), start_number, end_number)
     
     if left < right:
         supported_moves.extend([SupportedMove.TURN_LEFT] * left)
@@ -21,7 +22,29 @@ def get_turns(start, end):
         supported_moves.extend([SupportedMove.TURN_LEFT] * left)
     
     return supported_moves
-        
+
+def get_direction_number(direction):
+    if direction[0] == -1:
+        if direction[1] == 1:
+            return 8
+        elif direction[1] == 0:
+            return 7
+        elif direction[1] == -1:
+            return 6
+    elif direction[0] == 0:
+        if direction[1] == 1:
+            return 1
+        elif direction[1] == -1:
+            return 5
+    elif direction[0] == 1:
+        if direction[1] == 1:
+            return 2
+        elif direction[1] == 0:
+            return 3
+        elif direction[1] == -1:
+            return 4
+    raise Exception("Direction x:{} y:{} not exists.".format(direction[0], direction[1]))
+    
 def turn_count(cycle_data, start, end):
     i = 0
     
