18659a05c4c3ad126af5ceb08eb17fd06e9c2cf5
Patrik Pavelcak <patrik.pavelcak@ibm.com>
2021-11-14 17:07:09 +0100

    fix: missing brackets

--- map.py
+++ map.py
@@ -55,7 +55,7 @@ class Map:
         self.set_direction(direction)
 
         # there already is something on this position and we're trying to overwrite - NOT ALLOWED
-        if self.get_current_position_state != position_state:
+        if self.get_current_position_state() != position_state:
             return False
 
         self.map[self.position] = position_state
